9b6264e3a0ede5a431e82f21ec0d646f45dd5277
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2026-08-16 05:58:29 -0400

    adds primary feature engineering zenml step

---
+++ src/vedika/orchestration/steps/feature_engineering.py
@@ -0,0 +1,32 @@
+from typing import Annotated
+
+from zenml import step
+
+from vedika.application.preprocessing.cleaning.dispatcher import CleaningDispatcher
+from vedika.domain.cleaned import BaseCleanedDomain
+from vedika.domain.raw import BaseContentDomain
+from vedika.infrastructure.db.factory import get_cleaned_repository
+
+
+@step
+def clean_document(
+    raw_documents: list[BaseContentDomain],
+) -> Annotated[list[BaseCleanedDomain], "cleaned_documents"]:
+    """ZenML step: Takes raw documents, cleans those and persists in the corresponding DBs"""
+    cleaned_documents = []
+    for raw_doc in raw_documents:
+        cleaned_repo = get_cleaned_repository(category=raw_doc.category)
+        if cleaned_repo.exists_by_id(document_id=raw_doc.id):
+            cached_doc = cleaned_repo.get_by_id(document_id=raw_doc.id)
+            cleaned_documents.append(cached_doc)
+            continue
+
+        # Handoff to application layer
+        try:
+            cleaned_doc = CleaningDispatcher.dispatch(data=raw_doc)
+            cleaned_repo.save(document=cleaned_doc)
+            cleaned_documents.append(cleaned_doc)
+        except Exception as e:
+            raise e
+    return cleaned_documents
+
