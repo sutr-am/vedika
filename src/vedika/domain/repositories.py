e21c10d1af06844e1d533de95e0161a8c30760a6
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2026-08-16 05:50:41 -0400

    update repositories and db infrastructure

--- src/vedika/domain/repositories.py
+++ src/vedika/domain/repositories.py
@@ -1,30 +1,52 @@
 from abc import ABC, abstractmethod
 from typing import Generic, TypeVar
+from uuid import UUID
 
-from vedika.domain.chunks import BaseChunkDomain
-from vedika.domain.documents import BaseContentDomain, UserDomain
+from vedika.domain.chunked import BaseChunkDomain
+from vedika.domain.cleaned import BaseCleanedDomain
+from vedika.domain.raw import BaseContentDomain, UserDomain
 
 # A generic type that is of DocumentDomain or its subclass
-T = TypeVar("T", bound=BaseContentDomain)
-U = TypeVar("U", bound=UserDomain)
+ContentDomainType = TypeVar("ContentDomainType", bound=BaseContentDomain)
+UserDomainType = TypeVar("UserDomainType", bound=UserDomain)
+CleanedDomainType = TypeVar("CleanedDomainType", bound=BaseCleanedDomain)
+
+
 C = TypeVar("C", bound=BaseChunkDomain)
 
 
-class BaseContentRepository(Generic[T], ABC):
+class BaseContentRepository(Generic[ContentDomainType], ABC):
     @abstractmethod
     def exists_by_url(self, url: str) -> bool:
         """Check if a document is already stored without fetching the full payload."""
         pass
 
     @abstractmethod
-    def save(self, document: T) -> None:
+    def save(self, document: ContentDomainType) -> None:
         """Saves a document to the underlying storage"""
         pass
 
 
-class BaseUserRepository(Generic[U], ABC):
+class BaseUserRepository(Generic[UserDomainType], ABC):
     @abstractmethod
-    def get_or_create_user(self, first_name: str, last_name: str) -> U:
+    def get_or_create_user(self, first_name: str, last_name: str) -> UserDomainType:
+        pass
+
+
+class BaseCleanedRepository(Generic[CleanedDomainType], ABC):
+    @abstractmethod
+    def exists_by_id(self, document_id: UUID) -> bool:
+        """Checks if a cleaned document already exists by its original **document_id**"""
+        pass
+
+    @abstractmethod
+    def save(self, document: CleanedDomainType) -> None:
+        """Saves a cleaned document to its storage"""
+        pass
+
+    @abstractmethod
+    def get_by_id(self, document_id: UUID) -> CleanedDomainType | None:
+        """Fetches a cleaned domain document by its ID"""
         pass
 
 
