00e9f200776b07c0601ddf6bdc6efdadab1ab112
Vinay Kumar <12234745+imflash217@users.noreply.github.com>
2026-08-23 17:54:09 -0700

    fix: update etl and feature engineering scripts

--- tools/run_feature_engineering.py
+++ tools/run_feature_engineering.py
@@ -4,7 +4,7 @@ from vedika.domain.types import DataCategory
 from vedika.orchestration.pipelines.feature_engineering import feature_engineering_pipeline
 
 if __name__ == "__main__":
-    logger.info("triggering the Feature Engineering PIepline")
+    logger.info("triggering the Feature Engineering Pipeline")
     pipeline_instance = feature_engineering_pipeline.with_options(enable_cache=False)
     pipeline_instance(category=DataCategory.CODEBASES)
     logger.info(f"\n{'---' * 30}\n")
